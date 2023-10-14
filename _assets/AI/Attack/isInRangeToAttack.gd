d142d296c3f9cca919d9958926610fcac0ad3717
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2023-10-14 17:14:59 -0500

    Trying without beehave to see if build works

--- _assets/AI/Attack/isInRangeToAttack.gd
+++
@@ -1,7 +0,0 @@
-class_name IsInRangeToAttack extends ConditionLeaf
-
-
-func tick(actor:Node, blackboard:Blackboard) -> int:
-	if blackboard.has_value("attack_range") && blackboard.has_value("target"):
-		return SUCCESS
-	return FAILURE
